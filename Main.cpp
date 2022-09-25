fcecf18f83a35f0e26aa883b1636028115aef307
Gabozx <gabriel.zuleta@alumnos.ucn.cl>
2022-09-25 18:44:10 -0300

    only main changes: prints and functions

--- Main.cpp
+++ Main.cpp
@@ -3,35 +3,102 @@ using namespace std;
 
 void consulta()
 {
-    int respuesta;
+    cout<<"------------: CONSULTA :------------"<<endl;
     cout<<  "Que tipo de consulta desea hacer?\n";
-    cout<<  "1. Consultar un ramo\n";
-    cout<<  "2. Consultar un profesor\n";
-    cout<<  "3. Consultar un alumno\n";
-    cout<<  "presione cualquier otra numero para salir"<<endl;
-    cout<<  "ADIOS  \n";
-    cin>> respuesta;
-    switch (respuesta) {
+    cout<<  "1. Consultar por un ramo\n";
+    cout<<  "2. Consultar por un profesor\n";
+    cout<<  "3. Consultar por un alumno\n";
+    cout<<  "presione cualquier otro numero para salir"<<endl;
+    int res;
+    cout<<">"; cin>> res;
+    string nombreR;string nombreP;string nombreE;
+    switch (res)
+    {
         case 1:
-            //ramo
+            cout<<"ingrese nombre del ramo"<<endl;
+            cout<<">";cin>> nombreR;
+            //
             break;
 
         case 2:
-            //profesor
+            cout<<"ingrese nombre del profesor"<<endl;
+            cout<<">";cin>> nombreP;
+            //
             break;
 
         case 3:
-            //alumno
+            cout<<"ingrese nombre del Estudiante"<<endl;
+            cout<<">";cin>> nombreE;
+            //
             break;
     }
+
 }
 
-bool agregar(){
+void agregar()
+{
+    cout<<"------------: AGREGAR :------------"<<endl;
+    cout<<  "Que tipo de dato desea agregar al sistema?\n";
+    cout<<"1. Agregar un ramo\n";
+    cout<<"2. Agregar un Profesor\n";
+    cout<<"3. Agregar un alumno\n";
+    cout<<"presione cualquier otro numero para salir"<<endl;
+    int res;
+    cout<<">";cin>>res;
+    string nombreR;string nombreP;string nombreE;
+    switch (res)
+    {
+        case 1:
+            cout<<"ingrese nombre del ramo"<<endl;
+            cout<<">";cin>> nombreR;
+            //
+            break;
 
-    return false;
+        case 2:
+            cout<<"ingrese nombre del profesor"<<endl;
+            cout<<">";cin>> nombreP;
+            //
+            break;
+
+        case 3:
+            cout<<"ingrese nombre del Estudiante"<<endl;
+            cout<<">";cin>> nombreE;
+            //
+            break;
+    }
+
 }
 
-void modificar(){
+void modificar()
+{
+    cout<<"------------: MODIFICAR :------------"<<endl;
+    cout<<  "Que tipo de dato desea modificar en el sistema?\n";
+    cout<<"1. Modificar datos de un ramo\n";
+    cout<<"2. Modificar datos de un Profesor\n";
+    cout<<"3. Modificar datos de un alumno\n";
+    int res;
+    cout<<">";cin>>res;
+    string nombreR;string nombreP;string nombreE;
+    switch (res)
+    {
+        case 1:
+            cout<<"ingrese nombre del ramo"<<endl;
+            cout<<">";cin>> nombreR;
+            //
+            break;
+
+        case 2:
+            cout<<"ingrese nombre del profesor"<<endl;
+            cout<<">";cin>> nombreP;
+            //
+            break;
+
+        case 3:
+            cout<<"ingrese nombre del Estudiante"<<endl;
+            cout<<">";cin>> nombreE;
+            //
+            break;
+    }
 
 }
 
@@ -41,12 +108,13 @@ int main()
     int res;
     while(!salir)
     {
+        cout<<"------------: MENU :------------"<<endl;
         cout<<"Que tipo de operacion desea hacer?\n";
         cout<<"1. Realizar una consulta\n";
         cout<<"2. Agregar informacion al sistema\n";
         cout<<"3. Modificar informacion del sistema\n";
         cout<<"4. Salir"<<endl;
-        cin>>res;
+        cout<<">";cin>>res;
 
         switch(res)
         {
