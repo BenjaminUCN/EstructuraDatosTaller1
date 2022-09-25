c4491e8ace8fbc83f7a2922674f76041e0641b5d
SharkedUwu <jose.carrillo@alumnos.ucn.cl>
2022-09-25 18:24:20 -0300

    Se agrego texto al main

--- Main.cpp
+++ Main.cpp
@@ -1,45 +1,100 @@
 #include <iostream>
 using namespace std;
-
 void consulta()
 {
-    int respuesta;
+    string respuesta;
     cout<<  "Que tipo de consulta desea hacer?\n";
     cout<<  "1. Consultar un ramo\n";
     cout<<  "2. Consultar un profesor\n";
     cout<<  "3. Consultar un alumno\n";
-    cout<<  "presione cualquier otra numero para salir"<<endl;
-    cout<<  "ADIOS  \n";
+    cout<<  "4. Volver al menu   \n";
     cin>> respuesta;
     switch (respuesta) {
-        case 1:
+        case "1":
             //ramo
             break;
 
-        case 2:
+        case "2":
             //profesor
             break;
 
-        case 3:
+        case "3":
             //alumno
             break;
+            
+        case "4":
+            return;
+            
+        default:
+            cout<<  "Ingrese una opción válida  \n";
     }
 }
 
-bool agregar(){
+void agregar(){
+    
+    string respuesta;
+    cout<<  "Que desea agregar?  \n";
+    cout<<  "1. Agregar un ramo al sistema  \n";
+    cout<<  "2. Agregar un profesor al sistema  \n";
+    cout<<  "3. Agregar un alumno al sistema    \n";
+    cout<<  "4. Volver al menu   \n";
+    cin>> respuesta;
+    switch (respuesta) {
+        case "1":
+            //ramo
+            break;
 
-    return false;
+        case "2":
+            //profesor
+            break;
+
+        case "3":
+            //alumno
+            break;
+            
+        case "4":
+            return;
+            
+        default:
+            cout<<  "Ingrese una opción válida  \n";
+
 }
 
 void modificar(){
+    
+    string respuesta;
+    cout<<  "Que desea modificar?  \n";
+    cout<<  "1. Modificar un ramo del sistema  \n";
+    cout<<  "2. Modificar un profesor del sistema  \n";
+    cout<<  "3. Modificar un alumno del sistema    \n";
+    cout<<  "4. Volver al menu   \n";
+    cin>> respuesta;
+    switch (respuesta) {
+        case "1":
+            //ramo
+            break;
+
+        case "2":
+            //profesor
+            break;
+
+        case "3":
+            //alumno
+            break;
+            
+        case "4":
+            return;
+            
+        default:
+            cout<<  "Ingrese una opción válida  \n";
 
 }
 
 int main()
 {
-    bool salir = false;
+    bool condicion = true;
     int res;
-    while(!salir)
+    do
     {
         cout<<"Que tipo de operacion desea hacer?\n";
         cout<<"1. Realizar una consulta\n";
@@ -65,7 +120,7 @@ int main()
             case 4:
                 salir=true;
 
-        }
+        } while(condicion);
     }
     return 0;
  }
