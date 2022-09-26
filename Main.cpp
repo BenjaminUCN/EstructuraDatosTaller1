2d4b0b5f3a5a2361bbfdcb7328a410f4dc37de45
Gabozx <gabriel.zuleta@alumnos.ucn.cl>
2022-09-26 19:38:15 -0300

    only main changes: order

--- Main.cpp
+++ Main.cpp
@@ -6,53 +6,48 @@
 #include "funciones.h"
 using namespace std;
 
+NodeList<Estudent>* estudiantes = new NodeList<Estudent>();
+NodeList<Profesor>* profesores = new NodeList<Profesor>();;
+NodeList<Course>* cursos = new NodeList<Course>();;
+
+bool menu() {
+    int res;
+    cout << "------------: MENU :------------" << endl;
+    cout << "Que tipo de operacion desea hacer?\n";
+    cout << "1. Realizar una consulta\n";
+    cout << "2. Agregar informacion al sistema\n";
+    cout << "3. Modificar informacion del sistema\n";
+    cout << "4. Salir" << endl;
+    cout << ">";cin >> res;
+
+    switch (res) {
+        case 1:
+            consulta(estudiantes, profesores, cursos);
+            break;
+
+        case 2:
+            agregar(estudiantes, profesores, cursos);
+            break;
+
+        case 3:
+            modificar(estudiantes, profesores, cursos);
+            break;
+
+        case 4:
+            return true;
+
+        default:
+            cout << "Ingrese una opcion valida  \n";
+    }
+    return false;
+}
+
 int main()
 {
+    bool salir;
+    do{
+        salir=menu();
+    }while(!salir);
 
-    NodeList<Estudent>* estudiantes = new NodeList<Estudent>();
-    NodeList<Profesor>* profesores = new NodeList<Profesor>();;
-    NodeList<Course>* cursos = new NodeList<Course>();;
-    Estudent e_aux;
-    Course c_aux;
-    Profesor p_aux;
-
-
-    bool salir = false;
-    int res;
-    while(!salir)
-    {
-        cout<<"Posicion de estudiantes: " <<estudiantes<<endl;
-        cout<<"------------: MENU :------------"<<endl;
-        cout<<"Que tipo de operacion desea hacer?\n";
-        cout<<"1. Realizar una consulta\n";
-        cout<<"2. Agregar informacion al sistema\n";
-        cout<<"3. Modificar informacion del sistema\n";
-        cout<<"4. Salir"<<endl;
-        cout<<">";cin>>res;
-
-        switch(res)
-        {
-            case 1:
-                consulta(estudiantes, profesores, cursos);
-                break;
-
-            case 2:
-                agregar(estudiantes, profesores, cursos);
-                break;
-
-            case 3:
-                modificar(estudiantes, profesores, cursos);
-                break;
-
-            case 4:
-                salir=true;
-                break;
-
-            default:
-                cout<<  "Ingrese una opcion valida  \n";
-
-
-        }
-    }
     return 0;
  }
